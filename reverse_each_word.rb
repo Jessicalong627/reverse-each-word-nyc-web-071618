aa17daf11197ee42d108bba43f5c7b09a1e6c75d
Jessica Long <jessicalong627@gmail.com>
2018-07-04 02:21:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
-
+def reverse_each_word(sentence)
+  new_sentence = sentence.split(" ")
+  new_sentence.each do |x|
+    new_sentence << x.reverse
+  end
+  new_sentence.join(" ")
+end
 
 def reverse_each_word_two(word)
   word.split(" ")
