83f40977421b010d7adae9476b2cd5ba673aa428
Jessica Long <jessicalong627@gmail.com>
2018-07-04 02:29:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word_with_each(string)
   original_array.each do|string|
   return_array << string.reverse
   end
-  return_array.join("")
+  return_array.join(" ")
 end
 
 def reverse_each_word(string)
